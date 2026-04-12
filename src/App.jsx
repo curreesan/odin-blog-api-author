742a9cd7ae8cd02d6fa1f4e33af283af78c39cc7
curreesan <115443503+curreesan@users.noreply.github.com>
2026-04-12 20:04:00 +0530

    Add Context setup,routes setup, login check ProtectedRoute component for protected routes

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+//Pages
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
+//Components
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
